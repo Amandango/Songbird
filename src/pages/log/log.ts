179c9a2309b227192cd0e30340d4b86cff7947f7
Amanda Ngo <amanda.mai.ngo@gmail.com>
2018-07-23 13:33:41 +1200

    added week texts

--- src/pages/log/log.ts
+++ src/pages/log/log.ts
@@ -17,9 +17,10 @@ export class LogPage {
   public allWeekTexts: Texts[] = [];
   public lottieLoadingConfig: Object;
   public loading: boolean;
-  public failed: boolean; 
+  public failed: boolean;
   public truncating: boolean;
   public limit: number;
+  public streak: number;
 
   private animation: any;
 
@@ -28,16 +29,17 @@ export class LogPage {
     public getBaseUrl: getBaseUrl,
     public nativePageTransitions: NativePageTransitions) {
 
-      this.loading = false;
-      this.failed = false;
-      this.limit = 40;
-      this.truncating = true;
+    this.loading = false;
+    this.failed = false;
+    this.limit = 40;
+    this.truncating = true;
+    // this.streak = 0;
 
-      this.lottieLoadingConfig = {
-        path: "assets/animations/loading2.json",
-        autoplay: true,
-        loop: true
-      };
+    this.lottieLoadingConfig = {
+      path: "assets/animations/loading2.json",
+      autoplay: true,
+      loop: true
+    };
 
   }
 
@@ -46,13 +48,13 @@ export class LogPage {
   }
 
   swipeEvent(e) {
-    if(e.direction == '4'){
-       this.navCtrl.parent.select(1);
-    }    
+    if (e.direction == '4') {
+      this.navCtrl.parent.select(1);
+    }
   }
 
   getAudioList() {
-    if(localStorage.getItem("audiolist")) {
+    if (localStorage.getItem("audiolist")) {
       this.audioList = JSON.parse(localStorage.getItem("audiolist"));
       console.log(this.audioList);
     }
@@ -64,81 +66,101 @@ export class LogPage {
     this.getWeekTexts();
   }
 
-  getTexts(){
+  getTexts() {
     this.loading = true;
     this.failed = false;
     console.log(localStorage.getItem("Token"));
     this.http.get(this.getBaseUrl.getBaseUrl() + "/getTextsById?jwt=" + localStorage.getItem("Token"), {
     })
-    .subscribe(
-      result => {
-        this.loading = false;
-        this.failed = false;
-        this.allTexts = result.json();
-        console.log(this.allTexts.length);
-        console.log(this.allTexts);
-        console.log('we got them')
+      .subscribe(
+        result => {
+          this.loading = false;
+          this.failed = false;
+          this.allTexts = result.json();
+          console.log(this.allTexts.length);
+          console.log(this.allTexts);
+          console.log('we got them')
 
-        var sortedTexts = this.allTexts.sort((a: Texts, b: Texts) => {
-          if (a.createdOn < b.createdOn) {
-            return 1;
-          }
+          var sortedTexts = this.allTexts.sort((a: Texts, b: Texts) => {
+            if (a.createdOn < b.createdOn) {
+              return 1;
+            }
 
-          if (a.createdOn > b.createdOn) {
-            return -1
-          }
+            if (a.createdOn > b.createdOn) {
+              return -1
+            }
 
-          return 0;
-        });
-      },
-      error => {
-        console.log(error);
-        this.loading = false;
-        this.failed = true;
-      }
-    );
-
-    // this.http.get(this.getBaseUrl.getBaseUrl() + "/getWeekTextsById?jwt=" + localStorage.getItem("Token"), {
-    // })
-    // .subscribe(
-    //   result => {
-    //     this.allWeekTexts = result.json();
-    //     console.log(this.allWeekTexts);
-    //     this.loading = false;
-    //   },
-    //   error => {
-    //     console.log(error);
-    //     this.failed = true;
-    //     this.loading = false;
-    //   }
-    // );
+            return 0;
+          });
+        },
+        error => {
+          console.log(error);
+          this.loading = false;
+          this.failed = true;
+        }
+      );
   }
 
   getWeekTexts() {
     this.loading = true;
+    this.http.get(this.getBaseUrl.getBaseUrl() + "/getWeekTextsById?jwt=" + localStorage.getItem("Token"), {
+    })
+    .subscribe(
+      result => {
+        this.allWeekTexts = result.json();
+        console.log(this.allWeekTexts);
+        this.loading = false;
+      },
+      error => {
+        console.log(error);
+        this.failed = true;
+        this.loading = false;
+      }
+    );
   }
 
   ionViewWillLeave() {
 
     let options: NativeTransitionOptions = {
-       direction: 'up',
-       duration: 300,
-       slowdownfactor: 1,
-       slidePixels: 20,
-       iosdelay: 100,
-       androiddelay: 10,
-       fixedPixelsTop: 70,
-       fixedPixelsBottom: 60
-      };
-   
+      direction: 'up',
+      duration: 300,
+      slowdownfactor: 1,
+      slidePixels: 20,
+      iosdelay: 100,
+      androiddelay: 10,
+      fixedPixelsTop: 70,
+      fixedPixelsBottom: 60
+    };
+
     this.nativePageTransitions.slide(options)
-      .then( result => {
+      .then(result => {
         console.log('success' + result);
       })
       .catch(err => {
         console.log(err);
       });
-   
-   }
 
+  }
+
+  // getStreak() {
+  //   console.log('streakrunning');
+  //   this.http.get(this.getBaseUrl.getBaseUrl() + "/streak?jwt=" + localStorage.getItem("Token"), {
+  //   })
+  //     .subscribe(
+  //       result => {
+  //         this.loading = false;
+  //         this.failed = false;
+  //         var streaks = result.json();
+  //         console.log(streaks)
+
+  //       },
+
+  //       error => {
+  //         console.log(error);
+  //         this.loading = false;
+  //         this.failed = true;
+  //       }
+  //     )
+
+  // }
 }
